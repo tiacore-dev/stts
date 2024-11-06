2f99db2e8c0534998eeb306c16d5cd6b971986ca
Viktoria <v.s.shuvaeva@gmail.com>
2024-11-06 14:55:34 +0700

    port

--- run.py
+++ run.py
@@ -4,8 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-port = os.getenv('PORT')
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=5000)
