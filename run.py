27c0221765078098d422c5331661b7e43674a7b1
Viktoria <v.s.shuvaeva@gmail.com>
2024-11-15 14:46:31 +0700

    ports

--- run.py
+++ run.py
@@ -11,8 +11,8 @@ load_dotenv()
 #app, socketio = create_app()
 
 
-
+port = os.getenv('FLASK_PORT')
 
 if __name__ == "__main__":
     app, socketio= create_app()
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)
