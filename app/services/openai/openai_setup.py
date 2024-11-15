2967872bcd06bca070624c0aceaa006a1181581a
Viktoria <v.s.shuvaeva@gmail.com>
2024-11-15 19:41:53 +0700

    fix

--- app/services/openai/openai_setup.py
+++ app/services/openai/openai_setup.py
@@ -1,18 +1,15 @@
 import openai
-import requests
+import ssl
 import urllib3
 
 def init_openai(app):
-    # Настроим urllib3 для игнорирования SSL-сертификатов
+    # Создаем PoolManager с отключенной проверкой SSL-сертификатов
     http = urllib3.PoolManager(cert_reqs='CERT_NONE', assert_hostname=False)
     
-    # Создаем кастомный запрос с игнорированием SSL-сертификатов
-    session = requests.Session()
-    adapter = requests.adapters.HTTPAdapter(max_retries=3)
-    session.mount("https://", adapter)
-    
-    # Передаем кастомный session в openai
+    # Настроим openai для использования кастомного HTTP-клиента
     openai.api_key = app.config['OPENAI_API_KEY']
-    openai.api_base = 'https://api.openai.com/v1'
-    openai.request = openai.api_requestor.APIRequestor(session=session)  # Используем custom session
-    
+    openai.api_base = "https://api.openai.com/v1"
+
+    # Устанавливаем кастомный http клиент
+    openai.http = http
+
