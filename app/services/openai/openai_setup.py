3d59e69e4d449eff8796fdb8c4770a762b58dd2b
Viktoria <v.s.shuvaeva@gmail.com>
2024-11-15 19:26:11 +0700

    fix

--- app/services/openai/openai_setup.py
+++ app/services/openai/openai_setup.py
@@ -3,15 +3,20 @@ import openai
 import ssl
 import urllib3
 
-# Отключение проверки сертификатов для всех запросов openai
+"""# Отключение проверки сертификатов для всех запросов openai
 context = ssl.create_default_context()
 context.check_hostname = False
-context.verify_mode = ssl.CERT_NONE
+context.verify_mode = ssl.CERT_NONE"""
 
 
 def init_openai(app):
-    openai.api_key=app.config['OPENAI_API_KEY']
-    openai.api_requestor._request_class = urllib3.PoolManager(
-        ssl_context=context
-        )
+    # Настроим urllib3 для игнорирования SSL-сертификатов
+    http = urllib3.PoolManager(cert_reqs='CERT_NONE', assert_hostname=False)
+    
+    # Обновляем настройки openai для использования custom HTTP-клиента
+    openai.api_base = 'https://api.openai.com/v1'
+    openai.api_key = app.config['OPENAI_API_KEY']
+    
+    # Используем кастомный HTTP-клиент для всех запросов
+    openai.request = openai.api_requestor.APIRequestor(http)
     
