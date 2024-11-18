903e0f14f07e290c10131168b0572be42d187310
Viktoria <v.s.shuvaeva@gmail.com>
2024-11-18 17:12:20 +0700

    fix

--- run.py
+++ run.py
@@ -15,4 +15,4 @@ port = os.getenv('FLASK_PORT')
 
 if __name__ == "__main__":
     app, socketio= create_app()
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5064, debug=True)
