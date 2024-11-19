aa86b2db6e66de8d71632b6efba9e94c4e461e6b
Viktoria <v.s.shuvaeva@gmail.com>
2024-11-19 18:24:26 +0700

    fix socket

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -7,7 +7,7 @@ from .audio_route import audio_bp
 from .logs_route import logs_bp
 from .analysis_route import analysis_bp
 from .status_route import status_bp
-from .socket_route import socket_bp
+
 
 
 
@@ -21,4 +21,4 @@ def register_routes(app: Flask):
     app.register_blueprint(logs_bp)
     app.register_blueprint(analysis_bp)
     app.register_blueprint(status_bp)
-    app.register_blueprint(socket_bp)
+
