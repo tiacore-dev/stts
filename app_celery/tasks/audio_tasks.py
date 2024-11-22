efdd0733f01133cacf8cfdae8e11a47e59020f6d
Viktoria <v.s.shuvaeva@gmail.com>
2024-11-22 23:49:26 +0700

    volumes

--- app_celery/tasks/audio_tasks.py
+++ app_celery/tasks/audio_tasks.py
@@ -13,24 +13,24 @@ logger = logging.getLogger('chatbot')
 
 #@celery.task(name='process_and_upload_file_task')
 @shared_task
-def process_and_upload_file_task(file_path, user_id, file_name_input, login):
+def process_and_upload_file_task(temp_file_path, user_id, file_name_input, login):
     logger.info('starting task')
     from app.database.managers.audio_manager import AudioFileManager
     db = AudioFileManager()
     s3_manager = get_s3_manager()
     bucket_name = get_bucket_name()
     
-    file_extension = os.path.splitext(file_path)[1]
+    file_extension = os.path.splitext(temp_file_path)[1]
     full_file_name = f"{file_name_input}{file_extension}"
     s3_key = full_file_name.replace(' ', '_')
-    file_size = os.path.getsize(file_path)  # Получаем размер файла из локальной системы
+    file_size = os.path.getsize(temp_file_path)  # Получаем размер файла из локальной системы
 
     try:
         # Сохраняем информацию о файле в БД
         audio_id = db.add_audio_file(user_id, file_name_input, file_extension, file_size, bucket_name, s3_key)
 
         # Загружаем файл в S3
-        with open(file_path, 'rb') as file:
+        with open(temp_file_path, 'rb') as file:
             s3_manager.upload_fileobj(file, bucket_name, audio_id + s3_key)
 
 
@@ -47,8 +47,8 @@ def process_and_upload_file_task(file_path, user_id, file_name_input, login):
         logger.error(f"Ошибка при загрузке файла {file_name_input}: {e}", extra={'user_id': login})
 
         # Удаляем файл в случае ошибки, если он был создан
-        if os.path.exists(file_path):
-            os.remove(file_path)
+        if os.path.exists(temp_file_path):
+            os.remove(temp_file_path)
 
         return {
             'status': 'error',
